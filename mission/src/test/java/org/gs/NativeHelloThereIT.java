805c99b772e5fa43c2c901bdd54157e1fd14fcfe
ndar123 <samwelelsa@gmail.com>
2022-01-12 16:49:02 +0300

    first github quarkus project

---
+++ mission/src/test/java/org/gs/NativeHelloThereIT.java
@@ -0,0 +1,9 @@
+package org.gs;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloThereIT extends HelloThereTest {
+
+    // Execute the same tests but in native mode.
+}
